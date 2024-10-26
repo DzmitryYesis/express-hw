aa0155a7c76a6f5c9a2e7a0a9f4a860cf543bf31
DzmitryYesis <yesis1988@mail.ru>
2024-10-26 15:18:43 +0200

    fix api 2

--- src/db/db.ts
+++ src/db/db.ts
@@ -8,7 +8,7 @@ dotenv.config()
 const mongoURI = process.env.MONGO_URL || SETTINGS.MONGO_URL
 
 export const client: MongoClient = new MongoClient(mongoURI)
-const db = client.db(SETTINGS.DB_NAME);
+const db = client.db();
 export const blogsCollection = db.collection<TBlog>(SETTINGS.DB_COLLECTION_BLOGS_NAME);
 export const postsCollection = db.collection<TPost>(SETTINGS.DB_COLLECTION_POSTS_NAME);
 
